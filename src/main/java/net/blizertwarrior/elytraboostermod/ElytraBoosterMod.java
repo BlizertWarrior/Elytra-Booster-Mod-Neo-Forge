91289859c9afd6f24d48bc92ee04ae8c107d3c30
BlizertWarrior <idriss.mezzi12@gmail.com>
2025-05-06 16:20:37 +0200

    Finally ready for the first release! Fixed crash on normal Minecraft instances. Still gotta add support to more languages.

--- src/main/java/net/blizertwarrior/elytraboostermod/ElytraBoosterMod.java
+++ src/main/java/net/blizertwarrior/elytraboostermod/ElytraBoosterMod.java
@@ -3,11 +3,17 @@ package net.blizertwarrior.elytraboostermod;
 import com.mojang.logging.LogUtils;
 import net.blizertwarrior.elytraboostermod.item.ModCreativeModTabs;
 import net.blizertwarrior.elytraboostermod.item.ModItems;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import org.slf4j.Logger;
 
 @Mod(ElytraBoosterMod.MODID)
@@ -24,5 +30,28 @@ public class ElytraBoosterMod
         NeoForge.EVENT_BUS.register(this);
         ModItems.register(modEventBus);
         ModCreativeModTabs.register(modEventBus);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
     }
 }
